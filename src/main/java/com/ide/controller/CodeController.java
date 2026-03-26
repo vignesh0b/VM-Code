bfee79261d019d4fd96b70fb481ce59358aac0a5
Mathan Prashath R <madhanprasath.r12106@gmail.com>
2026-03-26 15:45:58 +0530

    Project Structure

--- src/main/java/com/ide/controller/CodeController.java
+++ src/main/java/com/ide/controller/CodeController.java
@@ -1,16 +1,15 @@
 package com.ide.controller;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
-//@RequestMapping("/api")
+@Controller
 @ComponentScan("com.ide")
 public class CodeController {
-
     @GetMapping("/")
-    public String test() {
-        return "Backend is working ";
+    public String home() {
+        return "index";
     }
 }
